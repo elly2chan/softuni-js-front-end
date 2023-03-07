bdcfd98fef5dbddd030a5feb8b5d01b2264d6019
elly2chan <elena.iv.konstantinova@gmail.com>
2023-03-07 20:59:04 +0200

    Edit commit messages

--- week02/addAndSubtract.js
+++
@@ -1,8 +0,0 @@
-function addAndSubtract (num1, num2, num3) {
-    let sum = (num1, num2) => num1 + num2;
-    console.log(sum(num1, num2) - num3);
-}
-
-addAndSubtract(23, 6, 10);
-addAndSubtract(1, 17, 30);
-addAndSubtract(42, 58, 100);
--- week02/smallestOfThreeNumbers.js
+++
@@ -1,8 +0,0 @@
-function smallestOfNums (num1, num2, num3) {
-    console.log(Math.min(num1, num2, num3));
-}
-
-smallestOfNums(2, 5, 3);
-smallestOfNums(600, 342, 123);
-smallestOfNums(25, 21, 4);
-smallestOfNums(2, 2, 2);
