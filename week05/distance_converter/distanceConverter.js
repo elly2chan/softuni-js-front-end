b7aab24bb2b6297b121bc08c4ed78e18aaa5601f
elly2chan <elena.iv.konstantinova@gmail.com>
2023-12-14 15:20:52 +0200

    Adding tasks from week 5

--- week05/distance_converter/distanceConverter.js
+++ week05/distance_converter/distanceConverter.js
@@ -1,3 +1,27 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    button = document.getElementById('convert');
+    button.addEventListener('click', convert);
+
+    units = {
+        "km": 1000,
+        "m": 1,
+        "cm": 0.01,
+        "mm": 0.001,
+        "mi": 1609.34,
+        "yrd": 0.9144,
+        "ft": 0.3048,
+        "in": 0.0254
+    }
+
+    function convert() {
+        input = Number(document.getElementById('inputDistance').value);
+        inputUnits = document.getElementById('inputUnits').value;
+        output = document.getElementById('outputDistance');
+        outputUnits = document.getElementById('outputUnits').value;
+        
+        inputUnitsValue = units[inputUnits];
+        outputUnitsValue = units[outputUnits];
+        res = inputUnitsValue * input / outputUnitsValue;
+        output.value = res;
+    }
 }
