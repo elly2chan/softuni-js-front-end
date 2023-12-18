35f0c47535740ee38064be1a47387a1da5efe059
elly2chan <elena.iv.konstantinova@gmail.com>
2023-12-18 15:51:06 +0200

    Adding tasks from week 6

--- week06/asynchronous_programming/01.Bus-Stop/app.js
+++ week06/asynchronous_programming/01.Bus-Stop/app.js
@@ -1,3 +1,22 @@
 function getInfo() {
-    // TODO:
+    let stopId = Number(document.getElementById('stopId').value);
+    const BASE_URL = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    buses = document.getElementById('buses');
+
+    fetch(BASE_URL, {method: 'GET'})
+    .then(response => response.json())
+    .then(data => {
+        buses.innerHTML = '';
+        document.getElementById('stopName').textContent = data.name;
+        console.log(data.buses);
+        for ([busId, time] of Object.entries(data.buses)) {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${busId} arrives in ${time} minutes`
+            buses.appendChild(li);
+        }
+    })
+    .catch(() => {
+        document.getElementById('stopName').textContent = 'Error';
+    })
 }
--- week06/asynchronous_programming/02.Bus-Schedule/app.js
+++ week06/asynchronous_programming/02.Bus-Schedule/app.js
@@ -1,10 +1,35 @@
 function solve() {
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/'
+    let info = document.getElementsByClassName('info')[0];
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+
+    let departId = 'depot';
+    let arriveStopName = '';
+
     function depart() {
-        // TODO: 
+        fetch(BASE_URL + departId, {method: 'GET'})
+        .then(response => response.json())
+        .then(data => {
+            info.textContent = `Next stop ${data.name}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+            departId = data.next;
+            arriveStopName = data.name;
+        })
+        .catch((err) => {
+            info.textContent = 'Error';
+        })
     }
 
     async function arrive() {
-        // TODO:
+        fetch(BASE_URL + 'depot', {method: 'GET'})
+        .then(response => response.json())
+        .then(data => {
+            departBtn.disabled = false;
+            arriveBtn.disabled = true;
+            info.textContent = `Arriving at ${arriveStopName}`;
+        })
     }
 
     return {
